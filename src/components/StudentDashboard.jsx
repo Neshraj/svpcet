b507c016fcdce4e36d9e6ac025d49f31bb7b3863
Neshraj <sneshrajappu@gmail.com>
2025-06-30 22:32:34 +0530

    Border added

--- src/components/StudentDashboard.jsx
+++ src/components/StudentDashboard.jsx
@@ -1,29 +1,18 @@
 import { useState } from "react";
 import TodayClasses from "./TodayClasses";
 
+function StudentDashboard() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
 
-function Bar() {
-
-    const [activeIndex, setActiveIndex] = useState(0);
-    const [isOpen, setIsOpen] = useState(false);
-    
-      const items = [
-        { icon: "/assets/logos/dashboard.svg", label: "Dashboard" },
-        { icon: "/assets/logos/accademics.svg", label: "Accademics" },
-        { icon: "/assets/logos/timetable.svg", label: "Time Table" },
-        { icon: "/assets/logos/attandense.svg", label: "Attandance" },
-        { icon: "/assets/logos/profile2.svg", label: "Profile" },
-        { icon: "/assets/logos/logout.svg", label: "Logout" },
-      ];
-
-      
-
-
-
-
-
-
-
+  const items = [
+    { icon: "/assets/logos/dashboard.svg", label: "Dashboard" },
+    { icon: "/assets/logos/accademics.svg", label: "Accademics" },
+    { icon: "/assets/logos/timetable.svg", label: "Time Table" },
+    { icon: "/assets/logos/attandense.svg", label: "Attandance" },
+    { icon: "/assets/logos/profile2.svg", label: "Profile" },
+    { icon: "/assets/logos/logout.svg", label: "Logout" },
+  ];
 
   return (
     <>
@@ -31,41 +20,43 @@ function Bar() {
         {/* Head */}
         <div className="container mx-auto p-3 flex flex-row justify-between items-center shadow-sm">
           <img
-        className={`w-6 h-6 transition-transform duration-300 cursor-pointer ${!isOpen ? 'rotate-360' : ''}`}
-        src="/assets/logos/menu.svg"
-        alt="Menu"
-        onClick={() => setIsOpen(!isOpen)}
-      />
+            className={`w-6 h-6 transition-transform duration-300 cursor-pointer ${
+              !isOpen ? "rotate-360" : ""
+            }`}
+            src="/assets/logos/menu.svg"
+            alt="Menu"
+            onClick={() => setIsOpen(!isOpen)}
+          />
           <p className="font-thin text-sm">Student Dashboard</p>
         </div>
 
         {/* Main Body */}
 
         <div className="container mx-auto flex flex-row mt-2">
-            {/* Side Bar */}
+          {/* Side Bar */}
           <div>
             <ul
-  className={`list-none overflow-hidden transition-all duration-400 ease-in-out 
-    ${isOpen ? 'w-0 opacity-0' : 'w-30 opacity-100'}`}
->
-            {items.map((item, index) => (
-              <li
-              className={`flex flex-row justify-star items-center rounded-md pr-7 py-2 mb-5 mr-3 overflow-hidden transition-all duration-100 ease-in-out hover:underline hover:bg-gray-100 hover:cursor-pointer ${
-                  activeIndex === index
-                    ? "bg-gray-200"
-                    : ""
-                }`}
-                key={index}
-                onClick={() => setActiveIndex(index)}
-              >
-                <img className="w-5 h-5" src={item.icon} alt={item.label} />
-                <p className="text-sm font-thin whitespace-nowrap pl-1">{item.label}</p>
-              </li>
-            ))}
-          </ul>
+              className={`list-none overflow-hidden pl-2 transition-all duration-400 ease-in-out 
+    ${isOpen ? "w-0 opacity-0" : "w-33 opacity-100"} md:pl-0`}
+            >
+              {items.map((item, index) => (
+                <li
+                  className={`flex flex-row justify-star items-center rounded-md pr-7 py-2 mb-5 mr-3 overflow-hidden transition-all duration-100 ease-in-out hover:underline hover:bg-gray-100 hover:cursor-pointer ${
+                    activeIndex === index ? "bg-gray-200" : ""
+                  }`}
+                  key={index}
+                  onClick={() => setActiveIndex(index)}
+                >
+                  <img className="w-5 h-5" src={item.icon} alt={item.label} />
+                  <p className="text-sm font-thin whitespace-nowrap pl-1">
+                    {item.label}
+                  </p>
+                </li>
+              ))}
+            </ul>
           </div>
 
-            {/* Dashboard Body */}
+          {/* Dashboard Body */}
           <div className="container mx-auto">
             <TodayClasses />
           </div>
@@ -75,4 +66,4 @@ function Bar() {
   );
 }
 
-export default Bar;
+export default StudentDashboard;
